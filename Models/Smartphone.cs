ba9fd2080d091bd62c9a94a24f92ee0edf22b81d
Miguel Neto <mlsneto@hotmail.com>
2024-01-08 06:01:18 -0300

    Implementando os parâmetros para as propriedades Modelo, IMEI e Memoria.

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -8,10 +8,13 @@ namespace DesafioPOO.Models
         private string IMEI { get; set; }
         private int Memoria{get; set;}
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
         }
 
         public void Ligar()
