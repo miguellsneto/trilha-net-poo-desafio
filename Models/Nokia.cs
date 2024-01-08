d42c6f5cabb8aee1214d1a80e16ef1c603d376ca
Miguel Neto <mlsneto@hotmail.com>
2024-01-08 06:16:40 -0300

    Herdando a classe Smartphone na classe Iphone

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -4,9 +4,8 @@ namespace DesafioPOO.Models
     public class Nokia : Smartphone
     {
         public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
-    {
-
-    }
+        {
+        }
         // TODO: Sobrescrever o método "InstalarAplicativo"
         public override void InstalarAplicativo(string nomeApp)
         {
